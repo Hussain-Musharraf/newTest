f53539c6eb1d732225e4ca29101528bd4b43577b
Hussain-Musharraf <musharrafhussain4397@gmail.com>
2022-12-22 18:34:05 +0530

    changes

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-const btn=document.getElementById("btn");
-btn.addEventListener("click",fnct);
-function fnct(){
-    alert("You've clicked Me");
-}
+// const btn=document.getElementById("btn");
+// btn.addEventListener("click",fnct);
+// function fnct(){
+//     alert("You've clicked Me");
+// }
+
+
+
+// function btnDivs(){
+//     const btnD=document.getElementById("btnDiv");
+//     for(let i=0;i<20;i++){
+//         const btnElement=document.createElement("Button");
+//         btnElement.innerHTML=`Button ${i+1}`;
+//         btnElement.classList.add("btnelmnt");
+//         btnD.appendChild(btnElement);
+//     }
+// }
+// btnDivs();
+
+
+
+
+
+
+
+// const number=[1,1,2,2,3,3,4,2,3,5,1,3,1,2,5,1,3,8,9,7,2,0,6,7];
+// console.log(number);
+// const uniqueNumbers=new Set(number);
+// console.log(uniqueNumbers);
+
+// const names=['Musharraf','John','Musharraf'];
+// console.log(names);
+// const uniqueNames=new Set(names);
+// console.log(uniqueNames);
+
